dd7fbb8ec376d43bb5201c020eaf992bfbc2478e
Calamanthus <joel.allan@sa.gov.au>
2025-03-20 12:57:37 +1030

    updated clusts function parameters & info

--- R/clusts.R
+++ R/clusts.R
@@ -3,39 +3,51 @@
 #' Can be used as a precursor to make_mcp or make_ahull for creating refined distribution polygons
 #' for species with geographically separated populations/subspecies.
 #'
-#' @param presence Cleaned and filtered dataframe of presences
+#' @param presence Cleaned and filtered data frame of presences.
 #' @param h Distance used to cluster points, i.e. points within individual clusters
 #' will not be further apart than this distance.
+#' @param pres_x,pres_y Character. Name of the columns in `presence` that have
+#' the x and y coordinates.
 #'
-#' @return data frame saved as rds to `out_file` path containing environ and land_pc (percent land overlap ) fields for the taxa.
+#' @return Vector of cluster ID numbers for each row in `presence`,
+#' indicating which cluster each row belongs.
 #'
 #' @export
 #'
 
-clusts <- function(presence, h, ...){
+clusts <- function(presence
+                   , h
+                   , pres_x = "long"
+                   , pres_y = "lat"
+                   , ...
+){
 
-  if(nrow(presence)>1){
+  prep <- presence %>%
+    dplyr::mutate(rownames = dplyr::row_number()) |>
+    dplyr::select(rownames, !!rlang::ensym(pres_x), !!rlang::ensym(pres_y))
 
-    prep <- presence %>%
-      dplyr::mutate(rownames = dplyr::row_number()) |>
-      dplyr::select(rownames,X,Y)
+  if(nrow(prep)>1){
 
     # Using hierarchical clustering on a distance matrix to create a dendogram
-    dend <- hclust(dist(prep), method="single")
+    dend <- hclust(dist(prep), method = "single")
 
     # Cut tree to obtain clusters within specified distance
-    clust <- data.frame(rownames = prep$rownames
-                        , clust = cutree(dend, h = h)) # Distance with a 50000 m threshold
+    clust <- tibble::tibble(rownames = prep$rownames
+                            , clust = cutree(dend, h = h)) # Distance with a 50000 m threshold
 
-  } else { # if only one species record in landscape (can't do clustering above)
+  } else { # if only one unique occurrence, then can't do clustering above and only one cluster.
 
-    clust <- data.frame(rownames = 1
-                        , clust = 1
-    )
+    clust <- prep |>
+      dplyr::mutate(rownames = 1
+                    , clust = 1
+      )
+
   }
 
   clust <- clust %>%
     dplyr::arrange(rownames) %>%
     dplyr::pull(clust)
+
+  return(clust)
 
 }
