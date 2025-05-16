7b8ee2a39515a7730098febb892e295e47bfbb3c
Joel Allan <JAllan@env.sa.gov.au>
2025-05-16 15:45:51 +0930

    mcp overlap option

--- R/reg_taxa.R
+++ R/reg_taxa.R
@@ -21,7 +21,11 @@
 #' @param taxonomy Taxonomy object returned by envClean::make_taxonomy in relation to the taxa.
 #' Must have subspecies level taxonomy.
 #' @param remove Logical. Remove out of region taxa?
-
+#' @param use_mcp Logical. Use a minimum convex polygon (MCP) around presences to determine region overlap?
+#' @param mcp_file Character. If use_mcp == TRUE, optional file path of existing mcp to be used.
+#' If use_mcp == TRUE and mcp_file == NULL, a new mcp will be constructed from `presence`.
+#' Currently, only geoparquet files are accepted. Use sfarrow to convert sf objects to geoparquet.
+#'
 #' @return Data frame with species column, subspecies column, and 'pres_dist' & 'distrib_dist' columns indicating
 #' distance in metres from region to closest presence and distribution respectively. If remove == TRUE, rows with
 #' out of region subspecies will not appear.
@@ -40,7 +44,12 @@ reg_taxa <- function(presence
                      , buf = 0
                      , taxonomy
                      , remove = FALSE
+                     , use_mcp = FALSE
+                     , mcp_file = NULL
 ) {
+
+  region_bound <- region_bound %>%
+    {if(!is.null(use_crs)) sf::st_transform(., crs = use_crs) else .}
 
   # Distance of closest presence to region ----
   pres_dist <- presence %>%
@@ -63,7 +72,6 @@ reg_taxa <- function(presence
     dplyr::mutate(taxa = taxa)
 
   # Distance of distribution(s) to region ----
-
   if(!any(is.na(distrib_file)|is.null(distrib_file))){
 
     distrib_dist <- distrib_file %>%
@@ -92,11 +100,53 @@ reg_taxa <- function(presence
 
   }
 
+  # MCP overlap with region ----
+  if(use_mcp){
+
+    if(!is.null(mcp_file)) {
+
+      mcp <- sfarrow::st_read_parquet(mcp_file)
+
+    } else {
+
+      mcp <- envDistribution::make_mcp(presence = presence
+                                       , out_file = tempfile()
+                                       , force_new = FALSE
+                                       , pres_x = pres_x
+                                       , pres_y = pres_y
+                                       , in_crs = pres_crs
+                                       , out_crs = use_crs
+                                       , buf = 0
+                                       , clip = NULL
+      )
+
+    }
+
+    mcp_dist <- mcp |>
+      sf::st_transform(crs = use_crs) %>%
+      nngeo::st_nn(region_bound
+                   , .
+                   , k = 1
+                   , maxdist = Inf
+                   , returnDist = TRUE
+                   , progress = FALSE
+                   , parallel = 1
+      ) %>%
+      .$dist %>%
+      unlist() %>%
+      tibble::as_tibble_col(column_name = "mcp_dist") %>%
+      dplyr::mutate(taxa = taxa)
+
+  }
+
   # Relevant taxa to region ----
   pres_distrib <- pres_dist %>%
-    {if(!any(is.na(distrib_file)|is.null(distrib_file))) dplyr::full_join(., distrib_dist) %>%
-        dplyr::mutate(in_region = pres_dist <= buf|distrib_dist <= buf)
-      else dplyr::mutate(., in_region = pres_dist <= buf)
+    dplyr::mutate(in_region = pres_dist <= buf) %>%
+    {if(!any(is.na(distrib_file)|is.null(distrib_file))) dplyr::full_join(., distrib_dist)
+      else dplyr::mutate(., in_region = distrib_dist <= buf|in_region)
+    } %>%
+    {if(use_mcp) dplyr::full_join(., mcp_dist)
+      else dplyr::mutate(., in_region = mcp_dist <= buf|in_region)
     } %>%
     dplyr::select(tidyr::any_of(c("taxa", "pres_dist", "distrib_dist", "in_region"))) %>%
     dplyr::distinct() %>%
