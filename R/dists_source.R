74c14c79b11a35a458df5df514ddc0a798c2f653
= <=>
2026-02-24 20:33:36 +1030

    add haven to default sources

--- R/dists_source.R
+++ R/dists_source.R
@@ -33,7 +33,7 @@
 #'
 
 dists_source <- function(distrib_dir = fs::path("H:", "data"),
-                         sources = c("epbc", "expert", "bsa", "seed_centre", "fsa", "ssp", "redlist"),
+                         sources = c("epbc", "expert", "bsa", "seed_centre", "fsa", "ssp", "redlist", "haven"),
                          source_scales = NULL,
                          standardise_taxonomy = FALSE,
                          target_ranks = c("species", "subspecies"),
